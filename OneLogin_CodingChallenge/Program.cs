206178803231efb60ddf95289ce16d4011b7680b
CarleeSinton <carleen.sinton@gmail.com>
2019-11-25 19:13:25 -0800

    Add project files.

---
+++ OneLogin_CodingChallenge/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace OneLogin_CodingChallenge
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Calculator();
+            // Things to consider: 
+                // order of operations
+                // fractions vs whole numbers
+                // returning mixed numbers (9/8 -> 1_1/8) OR finalDenominator = 1
+                // reducing fraction (2/8 -> 1/4)
+                // negative numbers?
+            
+        }
+        public static string Calculator()
+        {
+            Console.WriteLine("Hello! Please enter your equation: ");
+            Console.Write("? ");
+            string equation = Console.ReadLine();
+
+            //Get parts of equation
+            string[] equationParts = equation.Split(' ');
+            string givenOperator = equationParts[1];
+            string operand1 = equationParts[0];
+            int numerator1;
+            int denominator1;
+            int numerator2;
+            int denominator2;
+            if (operand1.Contains("_"))
+            {
+                string[] operand1Parts = operand1.Split('_','/');
+                int wholeNumber1 = Int32.Parse(operand1Parts[0]);
+                int tempNumerator1 = Int32.Parse(operand1Parts[1]);
+                denominator1 = Int32.Parse(operand1Parts[2]);
+                numerator1 = tempNumerator1 + (denominator1 * wholeNumber1);
+            }
+            else if (operand1.Contains("/"))
+            {
+                string[] operand1Parts = operand1.Split('/');
+                numerator1 = Int32.Parse(operand1Parts[0]);
+                denominator1 = Int32.Parse(operand1Parts[1]);
+            }
+            else
+            {
+                numerator1 = Int32.Parse(operand1);
+                denominator1 = 1;
+            }
+
+            string operand2 = equationParts[2];
+            if (operand2.Contains("_"))
+            {
+                string[] operand2Parts = operand2.Split('_', '/');
+                int wholeNumber2 = Int32.Parse(operand2Parts[0]);
+                int tempNumerator2 = Int32.Parse(operand2Parts[1]);
+                denominator2 = Int32.Parse(operand2Parts[2]);
+                numerator2 = tempNumerator2 + (denominator2 * wholeNumber2);
+            }
+            else if (operand2.Contains("/"))
+            {
+                string[] operand2Parts = operand2.Split('/');
+                numerator2 = Int32.Parse(operand2Parts[0]);
+                denominator2 = Int32.Parse(operand2Parts[1]);
+            }
+            else
+            {
+                numerator2 = Int32.Parse(operand2);
+                denominator2 = 1;
+            }
+
+
+            string result = "";
+            switch (givenOperator)
+            {
+                case "*":
+                    result = Multiply(numerator1, numerator2, denominator1, denominator2);
+                    break;
+                case "/":
+                    result = Divide(numerator1, numerator2, denominator1, denominator2);
+                    break;
+                case "+":
+                    result = Add(numerator1, numerator2, denominator1, denominator2);
+                    break;
+                case "-":
+                    result = Subtract(numerator1, numerator2, denominator1, denominator2);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+            }
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static string Multiply(int numerator1, int numerator2, int denominator1, int denominator2)
+        {
+            int newNumerator = numerator1 * numerator2;
+            int newDenominator = denominator1 * denominator2;
+            if (newNumerator/newDenominator > 0)
+            {
+                int wholeNumber = newNumerator / newDenominator;
+                newNumerator = newNumerator % newDenominator;
+                return $"= {wholeNumber}_{newNumerator}/{newDenominator}";
+            }
+            else
+            {
+                return $"= {newNumerator}/{newDenominator}";
+            }
+
+        }
+
+        public static string Divide(int numerator1, int numerator2, int denominator1, int denominator2)
+        {
+            int newNumerator = numerator1 * denominator2;
+            int newDenominator = denominator1 * numerator2;
+            return ($"= {newNumerator}/{newDenominator}");
+        }
+        public static string Add(int numerator1, int numerator2, int denominator1, int denominator2)
+        {
+            int newNumerator = (numerator1 * denominator2) + (denominator1 * numerator2);
+            int newDenominator = denominator1 * denominator2;
+            return ($"= {newNumerator}/{newDenominator}");
+        }
+        public static string Subtract(int numerator1, int numerator2, int denominator1, int denominator2)
+        {
+            int newNumerator = (numerator1 * denominator2) - (denominator1 * numerator2);
+            int newDenominator = denominator1 * denominator2;
+            return ($"= {newNumerator}/{newDenominator}");
+        }
+    }
+}
---
+++ OneLogin_CodingChallengeTests/ProgramTests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+
+namespace OneLogin_CodingChallenge.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        // Main method tests
+        public void MainTest(string equation, string expected)
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                OneLogin_CodingChallenge.Program.Calculator();
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(expected, result);
+            }
+        }
+        [TestMethod()]
+        public void MultiplyMainTest()
+        {
+            string equation = "? 1/2 * 3_3/4";
+            string expected = "= 1_7/8";
+            MainTest(equation, expected);
+        }
+        [TestMethod()]
+        public void DivideMainTest()
+        {
+            string equation = "? 1/2 / 3_3/4";
+            string expected = "= 4/30";
+            MainTest(equation, expected);
+        }
+        [TestMethod()]
+        public void AddMainTest()
+        {
+            string equation = "? 2_3/8 * 9/8";
+            string expected = "= 3_1/2";
+            MainTest(equation, expected);
+        }
+        [TestMethod()]
+        public void SubtractMainTest()
+        {
+            string equation = "? 2_3/8 - 9/8";
+            string expected = "= 1_1/4";
+            MainTest(equation, expected);
+        }
+
+        //Multiplication method tests
+        public void MultiplyMethodTest(int numerator1, int numerator2, int denominator1, int denominator2, string expected)
+        {
+            string result = OneLogin_CodingChallenge.Program.Multiply(numerator1, numerator2, denominator1, denominator2);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void MultiplyMethodTest1()
+        {
+            int numerator1 = 1;
+            int denominator1 = 2;
+            int numerator2 = 15;
+            int denominator2 = 4;
+            string expected = "1_7/8";
+            MultiplyMethodTest(numerator1, numerator2, denominator1, denominator2, expected);
+        }
+
+        // Division method tests
+        public void DivideMethodTest(int numerator1, int numerator2, int denominator1, int denominator2, string expected)
+        {
+            string result = OneLogin_CodingChallenge.Program.Divide(numerator1, numerator2, denominator1, denominator2);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void DivideMethodTest1()
+        {
+            int numerator1 = 1;
+            int denominator1 = 2;
+            int numerator2 = 15;
+            int denominator2 = 4;
+            string expected = "4/30";
+            DivideMethodTest(numerator1, numerator2, denominator1, denominator2, expected);
+        }
+
+        // Addition method tests
+        public void AddMethodTest(int numerator1, int numerator2, int denominator1, int denominator2, string expected)
+        {
+            string result = OneLogin_CodingChallenge.Program.Add(numerator1, numerator2, denominator1, denominator2);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void AddMethodTest1()
+        {
+            int numerator1 = 19;
+            int denominator1 = 8;
+            int numerator2 = 9;
+            int denominator2 = 8;
+            string expected = "3_1/2";
+            AddMethodTest(numerator1, numerator2, denominator1, denominator2, expected);
+        }
+
+        // Subtraction method tests
+        public void SubtractMethodTest(int numerator1, int numerator2, int denominator1, int denominator2, string expected)
+        {
+            string result = OneLogin_CodingChallenge.Program.Subtract(numerator1, numerator2, denominator1, denominator2);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void SubtractMethodTest1()
+        {
+            int numerator1 = 19;
+            int denominator1 = 8;
+            int numerator2 = 9;
+            int denominator2 = 8;
+            string expected = "1_1/4";
+            SubtractMethodTest(numerator1, numerator2, denominator1, denominator2, expected);
+        }
+    }
+}
